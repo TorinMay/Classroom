a61745ccdf7f7b3c36a3d75e949328f98e7220c5
TorinMay <mayte@s.dcsdk12.org>
2013-12-03 09:09:34 -0700

    Cannot get file for an unknown reason

--- classroom.java
+++ classroom.java
@@ -5,15 +5,21 @@ import java.io.*;
 class classroom {
 	public static void main(String[] args) throws IOException {
 	//imports file
+		// String[] names = new String[](students);
 		int student;
-		File students = new File("class.csv");
+		File students = new File("class.csv"); // WHY WON'T THIS WORK!!!!!!
 		Scanner scan = new Scanner(students);
 		student = scan.nextInt();
 		System.out.println(student);
+		while(scan.hasNextInt()) {
+			
+			System.out.println(student);
+		 }
 	}
 
 	public void randomStudent() {
 		Random r = new Random();
+
 
 	}
 }
